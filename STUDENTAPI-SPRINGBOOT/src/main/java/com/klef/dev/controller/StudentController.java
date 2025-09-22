12bfe1782ea962b6bee42ac695943a4b11346f10
SATII2004 <2300030457@kluniversity.in>
2025-09-22 18:59:07 +0530

    Project V2.0

--- STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/controller/StudentController.java
+++ STUDENTAPI-SPRINGBOOT/src/main/java/com/klef/dev/controller/StudentController.java
@@ -22,7 +22,14 @@ public class StudentController {
     @GetMapping("/")
     public String home() 
     {
-        return "Docker Full Stack Deployment Demo";
+        return "Docker Backend is running";
+    }
+
+
+    @GetMapping("/docker")
+    public String docker()
+    {
+        return "Docker Full Stack Deployment Demo with Git Actions" ;
     }
     
 
